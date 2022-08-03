03b5c82ed5b16883fd8a47e3ca5512e2ff5175a9
Emil Uhlin <emil.uhlin@se.experis.com>
2022-08-03 10:51:43 +0200

    Fix: removed duplicate files from previous branch

--- src/fundamentals/rpg_characters/characters/Rogue.java
+++
@@ -1,8 +0,0 @@
-package fundamentals.rpg_characters.characters;
-
-public class Rogue extends Character {
-    public Rogue(String _name) {
-        super(_name, 2, 6, 1,
-                1, 4, 1);
-    }
-}
--- src/fundamentals/rpg_characters/characters/Warrior.java
+++
@@ -1,8 +0,0 @@
-package fundamentals.rpg_characters.characters;
-
-public class Warrior extends Character {
-    public Warrior(String _name) {
-        super(_name, 5, 2, 1,
-                3, 2, 1);
-    }
-}
