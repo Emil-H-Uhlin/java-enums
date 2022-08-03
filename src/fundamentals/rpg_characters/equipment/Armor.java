212489d09a3dbe4dc9540c3bef9db4294be3ab67
Emil Uhlin <emil.uhlin@se.experis.com>
2022-08-03 13:50:32 +0200

    Feat: weapons always have item slot 'weapon', and armor cannot

--- src/fundamentals/rpg_characters/equipment/Armor.java
+++ src/fundamentals/rpg_characters/equipment/Armor.java
@@ -1,10 +1,22 @@
 package fundamentals.rpg_characters.equipment;
 
+import java.util.Arrays;
+
 public class Armor extends Equipment {
+    private static final EquipmentSlot[] allowedEquipmentSlots = {
+            EquipmentSlot.Head,
+            EquipmentSlot.Torso,
+            EquipmentSlot.Legs,
+    };
+
     public final ArmorType armorType;
 
     public Armor(String _name, EquipmentSlot _slot, ArmorType _armorType) {
         super(_name, _slot);
+
+        if (_slot == EquipmentSlot.Weapon) {
+            // throw exception
+        }
 
         armorType = _armorType;
     }
