161643a49b62cf17998f7f3c58443c7cd517245a
Emil Uhlin <emil.uhlin@se.experis.com>
2022-08-03 13:51:29 +0200

    Chore: removed unused array of accepted itemslots

--- src/fundamentals/rpg_characters/equipment/Armor.java
+++ src/fundamentals/rpg_characters/equipment/Armor.java
@@ -3,12 +3,6 @@ package fundamentals.rpg_characters.equipment;
 import java.util.Arrays;
 
 public class Armor extends Equipment {
-    private static final EquipmentSlot[] allowedEquipmentSlots = {
-            EquipmentSlot.Head,
-            EquipmentSlot.Torso,
-            EquipmentSlot.Legs,
-    };
-
     public final ArmorType armorType;
 
     public Armor(String _name, EquipmentSlot _slot, ArmorType _armorType) {
