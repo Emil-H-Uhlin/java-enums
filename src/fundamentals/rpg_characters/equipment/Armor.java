5b999d2cd874acc05cee9380c8ca9eda132beb8e
Emil Uhlin <emil.uhlin@se.experis.com>
2022-08-04 13:05:58 +0200

    Feat: added exception for armor with weapon slot assigned to them

--- src/fundamentals/rpg_characters/equipment/Armor.java
+++ src/fundamentals/rpg_characters/equipment/Armor.java
@@ -1,5 +1,7 @@
 package fundamentals.rpg_characters.equipment;
 
+import fundamentals.rpg_characters.exceptions.InvalidArmorException;
+
 import java.util.Arrays;
 
 public class Armor extends Equipment {
@@ -19,11 +21,11 @@ public class Armor extends Equipment {
     public void setBonusIntelligence(int _value) { bonusIntelligence = _value; }
 
     public Armor(String _name, EquipmentSlot _slot, int _requiredLevel, ArmorType _armorType,
-                 int _bonusStr, int _bonusDex, int _bonusInt) {
+                 int _bonusStr, int _bonusDex, int _bonusInt) throws InvalidArmorException {
         super(_name, _slot, _requiredLevel);
 
         if (_slot == EquipmentSlot.Weapon) {
-            // throw exception
+            throw new InvalidArmorException("An armor may not be assigned to slot 'Weapon'!");
         }
 
         armorType = _armorType;
