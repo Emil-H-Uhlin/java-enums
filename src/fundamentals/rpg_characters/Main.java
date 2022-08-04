982a7bdefafc1ef7d5f51cfef4956d2b878878f5
Emil Uhlin <emil.uhlin@se.experis.com>
2022-08-04 15:38:40 +0200

    Feat: added character sheet (as Character.toString())

--- src/fundamentals/rpg_characters/Main.java
+++ src/fundamentals/rpg_characters/Main.java
@@ -1,10 +1,7 @@
 package fundamentals.rpg_characters;
 
-import fundamentals.rpg_characters.characters.Character;
-import fundamentals.rpg_characters.characters.HeroClass;
-
 public class Main {
     public static void main(String[] args) {
-        var hero = new Character("Emil, the Destroyer", HeroClass.Warrior);
+
     }
 }
